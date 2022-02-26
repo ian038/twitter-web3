088de5d7b52b54a0347d656a80ed2421b39f0348
ian038 <ianphua3@gmail.com>
2022-02-26 16:05:40 -0500

    Added styles

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,16 @@
 import Image from 'next/image'
 
+const style = {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`,
+  loginContainer: `w-full h-full flex flex-col justify-center items-center pb-48`,
+  walletConnectButton: `text-2xl text-black bg-white font-bold mb-[-3rem] mt-[3rem] px-6 py-4 rounded-full cursor-pointer hover:bg-[#d7dbdc]`,
+  loginContent: `text-3xl font-bold text-center mt-24`,
+}
+
 const Home = () => {
   return (
-    <div>
+    <div className={style.wrapper}> 
       Hello world
     </div>
   )
