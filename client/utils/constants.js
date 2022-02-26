04de5e3070556cb9d9b660692d3437518cb38de8
ian038 <ianphua3@gmail.com>
2022-02-26 16:32:03 -0500

    Added sidebar

---
+++ client/utils/constants.js
@@ -0,0 +1,15 @@
+export const customStyles = {
+    content: {
+      top: '30%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: '',
+      padding: 0,
+      border: 'none'
+    },
+    overlay: {
+      backgroundColor: '#334250a7',
+    }
+}
